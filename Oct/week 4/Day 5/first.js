0d869ce4109301e42cbf8ee8e7e5efd5715ab7c2
aroprakash123 <aroprakashbarik68@gmail.com>
2025-10-27 23:16:41 +0530

    first js class

---
+++ Oct/week 4/Day 5/first.js
@@ -0,0 +1,84 @@
+- 
+    
+    //Java-Script
+    
+    //What is JavaScript?
+    
+    //Javascript is a high-level , object based ,programming language. Javascript is a interpreted, user friendly ,client-side scripting language.
+    
+    //History of javascript:
+    
+    //javascript developed by Brenden Eich-1995
+    
+    //First name of javascript
+    
+    //- -> MOCHA
+    //- -> next LIVESCRIPT
+    //- -> next--> JAVASCRIPT
+    
+    //FEATURES OF JAVASCRIPT:
+    
+    //1)Light Weight
+    
+    //2)intrepreted language
+    
+    //3)open source
+    
+    //4)cross platform
+    
+    //5)Dynamically typed language
+    
+    //6)Weakley typed language
+    
+    //7)client-side scripting language
+    
+    //8)Synchronous language
+    
+    //**Note**: The file extension for the javascript is .js
+    
+    /**Attaching the javascript file to html file:**
+    
+    We can attach the js file with html file in 2 ways.
+    
+    1.Internal Way
+    
+    2.External Way
+    
+    1.Internal Way
+    
+    By writing the code of javascript inside <script></script>tag
+    
+    Syntax:
+    
+    <head>
+    
+                <script>
+    
+                     //javascript code
+    
+                 </script>
+    
+    </head>
+    
+    2.External Way
+    
+    1. To write javascript externally to html file, we need to create an external javascript file with extension as .js (ex: index.js)
+    2. After that link that javascript file to the html by using script tag.
+    
+    Example:
+    
+    <body>
+    
+               <script    src=””></script>
+    
+    </body>
+    
+    **Printing Statements in javascript:**
+    
+    In javascript we have 2 types of printing statements
+    
+    1.document.write();  //  it will print the content in window
+    
+    2.console.log();  //  it will print the content in the console window
+    
+- Codes
