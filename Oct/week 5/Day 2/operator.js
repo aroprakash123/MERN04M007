aaf3b38610fa68ea326ced864bba9a768c53ae76
aroprakash123 <aroprakashbarik68@gmail.com>
2025-10-29 17:34:30 +0530

    Operator in JS

---
+++ Oct/week 5/Day 2/operator.js
@@ -0,0 +1,106 @@
+//Operators in JS
+//it is used to perform some operation on data
+
+//Arithmetic Operator 
+//  +,-,*,/
+//.Modulus
+//.Exponentiation
+//.Increment
+//.Decrement
+
+
+
+console.log("today we are going to learn operator")
+
+console.log(2+3)
+
+let a=2;
+let b=3;
+console.log((a+2)+3)
+console.log(typeof (a+2))
+console.log(typeof typeof (a+2))
+console.log(a-2)
+console.log(a*b)
+console.log(a/b)
+
+console.log(b%a);
+
+console.log(a**b)
+
+// var c;
+// console.log(c+2)//NaN-Not a number
+
+
+
+//Increment (Post,pre)
+//Decrement (post,pre)
+
+// console.log(a++)
+// console.log(a)
+
+// console.log(++a)
+
+// console.log(a--)
+// console.log(a)
+
+// console.log(--a)
+
+// let a; 
+// let c=a;
+// console.log(c) //undefined
+
+
+//Assignment Operator
+//=,
+
+
+// let c=a;
+//c=c+2;
+// c+=2;//4/
+// c-=2;
+// c%=2;
+// c**=2
+// console.log(c)
+
+
+//Comparision Operator
+
+console.log(a<b)
+console.log(a<=b)
+console.log(a>b)
+console.log(a>=b)
+
+
+let num1=2;
+let num2=2;
+// console.log(num1==num2)//type change karibani only check kariba(normal comparision)
+// console.log(num1==num2)//fasle heba because first datatype check kariba then value check kariba,datatype same na thile value check karibani(strict comparision operator)
+
+// console.log(num1 != num2);
+// console.log(num1 !== num2);
+
+
+//Logical Operators
+ //Logical AND && 2 ta true =true baki sabu false 
+ //logical OR ||  2 ta false thile false na hele true
+ //Logical NOT !
+
+
+console.log((3>2) && (4>5))
+console.log((3>2) || (4>5))
+console.log(!true)
+console.log(!false)
+
+console.log((num2++ >4) && (num1 === num2) || (num1-- != num2))
+
+
+// let n="1";
+// console.log(n)
+
+let str1="test";
+console.log(1+3-1+        +str1+4+ +"5-")
+
+
+
+
+
